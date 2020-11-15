a9590a0858daf0774ce53e37661aea7c52f838d9
DihanAbir <nahid.muradabir@gmail.com>
2020-11-15 19:27:52 +0600

    home commit

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h2>Here you can put other parts of home page</h2>
+            <h2>Here you can put other parts of home page from abir</h2>
         </div>
     );
 };
