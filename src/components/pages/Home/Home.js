6df63d1da703dbcbfa4c4f61c21d40b3a5b171cb
Fuad9 <alfuad8548@gmail.com>
2020-11-16 07:47:31 +0600

    new branch created

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,37 +1,52 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
+import Loading from "../../utilities/Loading";
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
+    const [hotelsInfo, setHotelsInfo] = useState([]);
 
-    //to add hotels data to database
-    const handleAddHotels = (e) => {
-        fetch("http://localhost:5000/addHotels", {
-            method: "POST",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(),
-        });
-    };
+    console.log(hotelsInfo);
 
-    // //to show hotels
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         try {
-    //             setLoading(true);
-    //             const response = await Axios.get("http://localhost:5000/showHotels");
-    //             setReviewsInfo(response.data);
-    //             setLoading(false);
-    //         } catch (error) {
-    //             setLoading(false);
-    //         }
-    //     };
-    //     getData();
-    // }, []);
+    // //to add hotels data to database
+    // const handleAddHotels = (e) => {
+    //     fetch("http://localhost:5000/addHotels", {
+    //         method: "POST",
+    //         headers: { "content-type": "application/json" },
+    //         body: JSON.stringify(hotelsData),
+    //     });
+    // };
+
+    //to show hotels
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setLoading(true);
+                const response = await Axios.get("http://localhost:5000/showHotels");
+                setHotelsInfo(response.data);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+            }
+        };
+        getData();
+    }, []);
 
     return (
         <div>
             <h2>Here you can put other parts of home page from abir</h2>
-            <button onClick={handleAddHotels}>add</button>
+            {loading ? (
+                <Loading />
+            ) : (
+                <div>
+                    {hotelsInfo.map((info) => (
+                        <div>
+                            <p>{info.priceDetails}</p>
+                            <img src={info.image1} alt="" />
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
